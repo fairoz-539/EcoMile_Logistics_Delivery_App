abd14ca1409f5ddb544f2878eff7988ad6a423dd
Fairoz Ahmed <129158093+fairoz-539@users.noreply.github.com>
2025-04-13 19:25:50 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,10 @@
 firebaseConfig = {
-    "apiKey": "AIzaSyAplFs0ZAnHraFkLWYu3-XT56EkqBI2oCk",
-    "authDomain": "ecomile-c72e9.firebaseapp.com",
-    "projectId": "ecomile-c72e9",
-    "storageBucket": "ecomile-c72e9.firebasestorage.app",
-    "messagingSenderId": "768310529493",
-    "appId": "1:768310529493:web:53552231f80f7a611d815c",
-    "measurementId": "G-HW8QLVELKK",
-    "databaseURL": "https://ecomile-c72e9-default-rtdb.firebaseio.com/"
+    "apiKey": "Your_apiKey",
+    "authDomain": "Your_authDomain",
+    "projectId": "Your_projectId",
+    "storageBucket": "Your_storageBucket",
+    "messagingSenderId": "Your_messagingSenderId",
+    "appId": "Your_appId",
+    "measurementId": "Your_measurementId",
+    "databaseURL": "Your_databaseURL"
 }
